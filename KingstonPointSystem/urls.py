48aa0d4af60233ce2d1e962acd2564c09dbc0360
Kumar Patel <kumar.patel@mmspos.com>
2016-05-11 17:04:12 -0300

    first module

--- KingstonPointSystem/urls.py
+++ KingstonPointSystem/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'KingstonPointSystem.views.home', name='home'),
+    url(r'^$', 'points.views.detail', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
